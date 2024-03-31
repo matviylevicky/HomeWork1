107897b50c7995650b6e58c4857861a180b3f0b5
matviylevicky <levicky2009@gmail.com>
2024-03-31 21:54:48 +0300

    проблема

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+//завдання номер 1
+let age = 14;
+console.log(age);
+
+//завдання номер 2
+let name = "Матвій";
+console.log(name);
+
+//завдання номер 3
+let isStudent = false;
+console.log(isStudent);
+
+//завдання номер 4
+let myString = "немає улюбленої цитати";
+console.log(myString);
+
+//завдання номер 5
+let myNumber = Symbol(15 + 10);
+console.log(myNumber);
+//або
+let myNumberTwo = 15;
+let numberSum = myNumberTwo + 10;
+console.log(numberSum);
+
+//завдання номер 6
+let myNull = null;
+console.log(myNull)
+//завдання номер 7
+// const message = "Введіть ваше ім'я";
+// let userMessage = prompt(message);
+// console.log(userMessage);
+// alert("Вітаємо: ",userMessage);
+
+//завдання номер 8
+const messageTwo = "Дія небезпечна";
+const userСhoice = confirm(messageTwo);
+let userTrue = true; {
+    alert("Дякую за підтвердження!");
+}
+
+let userFalse = false;
+alert("Дію відмінено!")
